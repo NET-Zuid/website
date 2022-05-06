de95f72e14ac501ab09901e46181dbbeb024abd8
Rick van den Bosch <rickvdbosch@outlook.com>
2022-05-06 17:57:16 +0200

    Initial commit: Models

---
+++ DNZ.Common.Models/ContentModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DNZ.Common.Models
+{
+    public class ContentModel
+    {
+        public string Description { get; set; }
+
+        public string HtmlContent { get; set; }
+
+        public string Title { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ DNZ.Common.Models/MeetingModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DNZ.Common.Models
+{
+    public class MeetingModel
+    {
+        public string Headline { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Description { get; set; }
+
+        public string Speaker { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime StartDateTime { get; set; }
+
+        public DateTime EndDateTime { get; set; }
+
+        public string Address { get; set; }
+
+        public int SubscriptionLimit { get; set; }
+    }
+}

596b76b0330edeb80c725b20e13ac5e9423fd241
Rick van den Bosch <rickvdbosch@outlook.com>
2022-05-23 22:50:10 +0200

    Implemented GetMeeting including mapping

--- DNZ.API/Functions/GetMeeting.cs
+++ DNZ.API/Functions/GetMeeting.cs
@@ -3,19 +3,36 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 
+using AutoMapper;
+
 using DNZ.Common.Models;
 using DNZ.Common.TableEntities;
 
 namespace DNZ.API.Functions
 {
-    public static class GetMeeting
+    public class GetMeeting
     {
-        [FunctionName("GetMeetingFunction")]
-        public static IActionResult Run(
+        #region Fields
+        
+        private readonly IMapper _mapper;
+
+        #endregion
+
+        #region Constructors
+
+        public GetMeeting(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        #endregion
+
+        [FunctionName(nameof(GetMeeting))]
+        public IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "meetings/{partitionKey}/{rowKey}")] HttpRequest req,
             [Table("meetings", "{partitionKey}", "{rowKey}", Connection = "storageConnectionString")] MeetingEntity meetingEntity)
         {
-            return new OkObjectResult(meetingEntity);
+            return new OkObjectResult(_mapper.Map<MeetingModel>(meetingEntity));
         }
     }
 }

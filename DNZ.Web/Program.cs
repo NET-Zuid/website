9ec20f5e624a216d81b09223e655ce4655398ae3
Rick van den Bosch <rickvdbosch@outlook.com>
2022-09-09 10:59:11 +0200

    Removed everything authentication from the project Managed (Azure AD B2C) auth via SWA

--- DNZ.Web/Program.cs
+++ DNZ.Web/Program.cs
@@ -9,9 +9,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddMsalAuthentication(options =>
-{
-    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-});
-
 await builder.Build().RunAsync();
